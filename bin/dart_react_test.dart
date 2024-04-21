1f71213fbebc9f620ef7e8812e0b4ad1145a5b5a
Vicente <busslina@gmail.com>
2024-04-21 08:57:09 +0200

    initial commit

---
+++ bin/dart_react_test.dart
@@ -0,0 +1,5 @@
+import 'package:dart_react_test/dart_react_test.dart' as dart_react_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_react_test.calculate()}!');
+}
---
+++ test/dart_react_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_react_test/dart_react_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
