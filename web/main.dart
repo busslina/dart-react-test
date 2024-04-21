2822fcbfa8e53dd307998183ad9a0343fad8b7e9
Vicente <busslina@gmail.com>
2024-04-21 09:08:32 +0200

    snapshot

---
+++ web/main.dart
@@ -0,0 +1,11 @@
+import 'dart:html';
+
+import 'package:dart_react_test/src/components/app.component.dart';
+import 'package:react/react_dom.dart';
+
+void main() {
+  render(
+    appComponent,
+    querySelector('#react_app_mount_point'),
+  );
+}
