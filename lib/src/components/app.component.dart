e8d2a52b5c2b99546f72bbca052781422985c404
Vicente <busslina@gmail.com>
2024-04-21 09:13:57 +0200

    snapshot

--- lib/src/components/app.component.dart
+++ lib/src/components/app.component.dart
@@ -1,14 +1,37 @@
+import 'dart:async';
+
 import 'package:react/react.dart';
 
 final appComponent = registerComponent2(AppComponent.new);
 
 class AppComponent extends Component2 {
+  AppComponent() {
+    print('AppComponent.constructor()');
+  }
+
+  @override
+  // TODO: implement initialState
+  Map get initialState => {
+        'title': 'Test app',
+      };
+
   @override
   render() => div(
         {},
         label(
           {},
-          'Test app',
+          state['title'],
         ),
       );
+
+  @override
+  void componentDidMount() {
+    super.componentDidMount();
+
+    Timer(const Duration(seconds: 10), () {
+      setState({
+        'title': 'New title',
+      });
+    });
+  }
 }
